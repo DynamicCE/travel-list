6da4d32f4772f69d92aa50fb3e14e57850a45628
Adınız <erkanmurat0632@gmail.com>
2023-12-10 01:15:40 +0300

    id should be uniq

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -26,7 +26,11 @@ function Logo() {
 }
 
 function Form() {
-  return <div className="add-form">what do you need for your trip</div>;
+  return (
+    <form className="add-form" action="">
+      what do you need for your trip
+    </form>
+  );
 }
 
 function PackingList() {
