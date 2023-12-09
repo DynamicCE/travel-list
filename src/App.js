ac906331057f9db085201c5a87715548206716bb
Adınız <erkanmurat0632@gmail.com>
2023-12-09 21:11:39 +0300

    static part of project is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+// COMPONENTS
+
+function Logo() {
+  return (
+    <div>
+      <h1> 🧳 Far Away 🏝️ </h1>
+    </div>
+  );
+}
+
+function Form() {
+  return <div className="add-form">what do you need for your trip</div>;
+}
+
+function PackingList() {
+  return <div className="list">list</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>you have x items on your list andyou already packed x </em>
+    </footer>
+  );
+}
